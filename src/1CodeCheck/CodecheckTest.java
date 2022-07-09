a5dc7725037b0556ce81dbc4e5c55e4b165648a0
psb-david-petty <david_petty@psbma.org>
2022-07-08 21:30:56 -0400

    Added use of Utilities methods and generated test data.

--- src/1CodeCheck/CodecheckTest.java
+++ src/1CodeCheck/CodecheckTest.java
@@ -14,44 +14,132 @@ import org.junit.Test;
 
 public class CodecheckTest {
 
-    // Powers of two and their squares.
-    private static int[][] squares = {
-        { 1, 1, }, { 2, 4, }, { 4, 16, }, { 8, 64, }, { 16, 256, }, { 32, 1024, }, { 64, 4096, }, { 128, 16384, }, { 256, 65536, }, { 512, 262144, }, { 1024, 1048576, }, { 2048, 4194304, }, { 4096, 16777216, }, { 8192, 67108864, }, { 16384, 268435456, }, { 32768, 1073741824, }, 
-    };
-    // Powers of two and their cubes.
-    private static int[][] cubes = {
-        { 1, 1, }, { 2, 8, }, { 4, 64, }, { 8, 512, }, { 16, 4096, }, { 32, 32768, }, { 64, 262144, }, { 128, 2097152, }, { 256, 16777216, }, { 512, 134217728, }, { 1024, 1073741824, }, 
-    };
+    private static int[][] squares, cubes;
 
     @Before
     public void setUp() {
+        // powers of two and their squares
+        squares = new int[][] {
+            {
+                0, 0,
+            },
+            {
+                1, 1,
+            },
+            {
+                2, 4,
+            },
+            {
+                4, 16,
+            },
+            {
+                8, 64,
+            },
+            {
+                16, 256,
+            },
+            {
+                32, 1024,
+            },
+            {
+                64, 4096,
+            },
+            {
+                128, 16384,
+            },
+            {
+                256, 65536,
+            },
+            {
+                512, 262144,
+            },
+            {
+                1024, 1048576,
+            },
+            {
+                2048, 4194304,
+            },
+            {
+                4096, 16777216,
+            },
+            {
+                8192, 67108864,
+            },
+            {
+                16384, 268435456,
+            },
+            {
+                32768, 1073741824,
+            },
+        };
+
+        // powers of two and their cubes
+        cubes = new int[][] {
+            {
+                0, 0,
+            },
+            {
+                1, 1,
+            },
+            {
+                2, 8,
+            },
+            {
+                4, 64,
+            },
+            {
+                8, 512,
+            },
+            {
+                16, 4096,
+            },
+            {
+                32, 32768,
+            },
+            {
+                64, 262144,
+            },
+            {
+                128, 2097152,
+            },
+            {
+                256, 16777216,
+            },
+            {
+                512, 134217728,
+            },
+            {
+                1024, 1073741824,
+            },
+        };
     }
 
     @After
     public void tearDown() {
+        squares = null; cubes = null;
     }
 
     @Test
     public void testSquares() {
-        System.out.printf("/////////////////// testSquares //////////////////\n");
+        System.out.printf("%s\n", Utilities.formatBanner("testSquares"));
         for (int i = 0; i < squares.length; i++) {
             int n = squares[i][0], nSquared = squares[i][1];
-            System.out.printf("square(%s) == %s\n",
+            System.out.printf("square(%s) \u2192 %s\n",
                 n, CodecheckSquare.square(n));
-            Assert.assertEquals("square(" + n + ")",
+            Assert.assertEquals(String.format("square(%s)", n),
                  nSquared, CodecheckSquare.square(n));
             }
     }
 
     @Test
     public void testCubes() {
-        System.out.printf("//////////////////// testCubes ///////////////////\n");
+        System.out.printf("%s\n", Utilities.formatBanner("testCubes"));
         for (int i = 0; i < cubes.length; i++) {
             int n = cubes[i][0], nCubed = cubes[i][1];
-            System.out.printf("cube(%s) == %s\n",
+            System.out.printf("cube(%s) \u2192 %s\n",
                 n, CodecheckCube.cube(n));
-            Assert.assertEquals("cube(" + n + ")",
-                 nCubed, CodecheckCube.cube(n));
+            Assert.assertEquals(String.format("cube(%s)", n),
+                nCubed, CodecheckCube.cube(n));
             }
     }
 }
