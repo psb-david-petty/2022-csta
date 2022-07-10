6743410af5eca1d99d4cefdecf3e39fc76983b8a
psb-david-petty <david_petty@psbma.org>
2022-07-10 17:23:50 -0400

    Initial commit.

---
+++ src/javadoc.sh
@@ -0,0 +1,9 @@
+dir=$(dirname $0)
+echo $dir
+javadoc -noqualifier all \
+  -linksource -sourcepath $dir/Utilities \
+  -d $dir/../doc/Utilities -link https://docs.oracle.com/javase/8/docs/api/ \
+  -windowtitle "2022 CSTA Unit Testing Workshop" \
+  -header "<p>2022 CSTA <a href=\"https://bit.ly/2022-csta-unit-testing-workshop\" target=\"_blank\">Unit Testing Workshop</a>" \
+  -footer "<p><a href=\"http://j.mp/psb_david_petty\" target=\"_blank\">David C. Petty</a>" \
+  $dir/Utilities/Format.java $dir/Utilities/Feedback.java
