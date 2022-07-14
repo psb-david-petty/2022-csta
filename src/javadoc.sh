b53b2fb86c170d44dab008ebe539ec8e530da540
psb-david-petty <david_petty@psbma.org>
2022-07-13 22:32:57 -0400

    Updated javadoc for Utilities to include Math.java.

--- src/javadoc.sh
+++ src/javadoc.sh
@@ -1,9 +1,10 @@
 # Generate javadoc for Utilities.
 dir=$(dirname $0)
+rm -rf $dir/../doc/Utilities
 javadoc -noqualifier all \
   -linksource -sourcepath $dir/Utilities \
   -d $dir/../doc/Utilities -link https://docs.oracle.com/javase/8/docs/api/ \
   -windowtitle "2022 CSTA Unit Testing Workshop" \
   -header "<p>2022 CSTA <a href=\"https://bit.ly/2022-csta-unit-testing-workshop\" target=\"_blank\">Unit Testing Workshop</a>" \
   -footer "<p><a href=\"http://j.mp/psb_david_petty\" target=\"_blank\">David C. Petty</a>" \
-  $dir/Utilities/Format.java $dir/Utilities/Feedback.java
+  $dir/Utilities/Format.java $dir/Utilities/Feedback.java $dir/Utilities/Math.java
